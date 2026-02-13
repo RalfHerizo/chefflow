71e3e083676f5519c596ba36a71f8d4261df0473
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-02-13 18:57:29 +0300

    feat(order): implement store method in OrderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Actions\SellProductAction;
+use Exception;
+
+class OrderController extends Controller
+{
+    public function store(Request $request, SellProductAction $sellProductAction){
+        
+        $validate_data = $request->validate([
+            'product_id'=>'required|exists: products,id',
+            'quantity'=>'required|integer|min:1'
+        ]);
+
+        try{
+
+            $product = Product::findOrFail($validate_datta['product_id']);
+            $sellProductAction->execute($product,$validate_data['quantity']);
+            
+            return back()->with('message','vente réussie! Stock mis à jour');
+
+        }catch(Exception $error){
+            return back()->withErrors([ 'error'=> $error->getMessage() ]);
+        }
+    }
+}
