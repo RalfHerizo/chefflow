808333b7d31456dd6601c206d06a7d04b7796d9b
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-02-13 12:25:26 +0300

    feat(test):add stockActionTest to test substract action in SellProductAction

---
+++ app/Actions/SellProductAction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
+class SellProductAction
+{
+    /**
+     * Exécute la vente d'un produit et déduit les stocks.
+     * @throws Exception
+     */
+
+     public function execute(Product $product, int $quantity): void
+     {
+        DB::transaction(function() use ($product, $quantity){
+            
+            if (!$product->is_active) {
+                throw new Exception("Disabled product");
+            }
+
+            foreach($product->ingredients as $ingredient){
+
+                $amountToSubstract = $ingredient->pivot->amount * $quantity;
+
+                if($ingredient->stock_quantity < $amountToSubstract){
+                    throw new Exception("Insufficient stock for the ingredient: {$ingredient->name}");
+                }
+                
+                $ingredient->decrement('stock_quantity', $amountToSubstract);
+            }
+
+        });
+     }
+}
---
+++ tests/Feature/StockActionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Ingredient;
+use App\Actions\SellProductAction;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('déduit les stocks d\'ingrédients lors d\'une vente', function () {
+    
+    $viande = Ingredient::create([
+        'name'=>'Viande hachée',
+        'unit'=>'g',
+        'stock_quantity'=>1000,
+        'alert_threshold' => 0,
+    ]);
+
+    $burger = Product::create([
+        'name'=>'Classic Burger',
+        'price'=>1200,
+        'is_active'=>true,
+        
+    ]);
+
+    $burger->ingredients()->attach($viande->id, ['amount'=>150]);
+
+    $burger->load('ingredients');
+
+    $action = new SellProductAction();
+    $action->execute($burger, 2);
+
+    $viande->refresh();
+
+    expect((float)$viande->stock_quantity)->toBe(700.0);
+});
