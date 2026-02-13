64054ceb3673b809b8441a8264a0a1c068e2530b
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-02-13 19:03:18 +0300

    feat(routes):add sell route for product

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sell',[OrderController::class,'store'])->name('products.sell');
