f8e29d111e29e00afc785572562a406c1025572f
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-03-23 09:48:34 +0300

    feat(landing): add welcome controller and route

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,11 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
@@ -32,6 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/orders/pos', [OrderController::class, 'pos'])->name('orders.pos');
+    Route::post('/orders',[OrderController::class,'store'])->name('orders.store');
     Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
     Route::get('/ingredients', [IngredientController::class, 'index'])->name('ingredients.index');
     Route::post('/ingredients', [IngredientController::class, 'store'])->name('ingredients.store');
@@ -46,5 +39,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 });
 
-Route::post('/orders',[OrderController::class,'store'])->name('orders.store');
 require __DIR__.'/auth.php';
